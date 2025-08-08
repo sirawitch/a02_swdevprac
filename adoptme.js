733e58b981cd25a55291911fdec72e68bb82b5d6
Sirawitch Tiyasuttipun <57455984+sirawitch@users.noreply.github.com>
2025-08-09 00:32:41 +0700

    Update

--- adoptme.js
+++ adoptme.js
@@ -1,14 +1,24 @@
 function checkData() {
-  let fname = document.getElementById("firstname");
-  let lname = document.getElementById("lastname");
-  let tel = document.getElementById("tel");
-  let npet = document.getElementById("petnumber");
+  let fname = document.getElementById("firstname").value.trim();
+  let lname = document.getElementById("lastname").value.trim();
+  let tel = document.getElementById("tel").value.trim();
+  let npet = parseInt(document.getElementById("petnumber").value);
   let checkbox = document.getElementById("checkbox");
-  if (fname == "" || lname == "" || tel == "") {
+
+  if (fname === "" || lname === "" || tel === "") {
     alert("First name, Last name and Tel No. can't be empty");
+    return;
   }
-  if (npet < 0 || npet > 100) {
-    alert("Number of pets only number between 0 and 100");
+
+  if (isNaN(npet) || npet < 0 || npet > 100) {
+    alert("Number of pets must be a number between 0 and 100");
+    return;
   }
-  console.log(checkbox);
+  if (!checkbox.checked) {
+    alert(
+      "Please note that submitting this form only expresses your interest. It does not guarantee that you will be selected to adopt a dog."
+    );
+    return;
+  }
+
 }
