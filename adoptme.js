5b439c7e97c5336709b611929b6a30c87d8ffdae
Sirawitch Tiyasuttipun <57455984+sirawitch@users.noreply.github.com>
2025-08-09 00:35:37 +0700

    remove return

--- adoptme.js
+++ adoptme.js
@@ -7,18 +7,15 @@ function checkData() {
 
   if (fname === "" || lname === "" || tel === "") {
     alert("First name, Last name and Tel No. can't be empty");
-    return;
   }
 
   if (isNaN(npet) || npet < 0 || npet > 100) {
     alert("Number of pets must be a number between 0 and 100");
-    return;
   }
   if (!checkbox.checked) {
     alert(
       "Please note that submitting this form only expresses your interest. It does not guarantee that you will be selected to adopt a dog."
     );
-    return;
   }
 
 }
